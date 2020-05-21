1f353795052e1d6752cc4738506e51268b534b3a
puwei <roy.wei@thoughtworks.com>
2020-05-21 16:27:09 +0800

    [Pu & Roy] Implemented Get method to pass the first Get test

--- src/main/java/cn/xpbootcamp/locker_robot/SmartLockerRobot.java
+++ src/main/java/cn/xpbootcamp/locker_robot/SmartLockerRobot.java
@@ -3,14 +3,19 @@ package cn.xpbootcamp.locker_robot;
 import cn.xpbootcamp.locker_robot.entity.Package;
 import cn.xpbootcamp.locker_robot.entity.Ticket;
 import cn.xpbootcamp.locker_robot.exception.AllLockersFullException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.UUID;
 
 public class SmartLockerRobot {
 
   private PriorityQueue<Locker> lockers;
 
+  private HashMap<UUID, Locker> packageLockerMapping;
+
   public SmartLockerRobot(List<Locker> lockers) {
+    packageLockerMapping = new HashMap<>(100);
     this.lockers = new PriorityQueue<>(
         (locker1, locker2) -> locker2.getAvailableSpace() - locker1.getAvailableSpace());
     lockers.forEach(locker -> this.lockers.offer(locker));
@@ -22,10 +27,13 @@ public class SmartLockerRobot {
       throw new AllLockersFullException();
     }
 
-    return locker.store(pack);
+    Ticket ticket = locker.store(pack);
+    packageLockerMapping.put(ticket.getPackageId(), locker);
+
+    return ticket;
   }
 
   public Package get(Ticket ticket) {
-    return null;
+    return packageLockerMapping.get(ticket.getPackageId()).get(ticket);
   }
 }
