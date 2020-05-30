1b120f51bc2b8d3d568e3f6fdeb5dff0c305e2e4
puwei <roy.wei@thoughtworks.com>
2020-05-30 22:02:14 +0800

    [Pu & Roy] Added more tests

--- src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
@@ -33,6 +33,42 @@ public class SuperLockerRobotTest {
     assertTrue(lockers.get(1).isPackageAvailable(ticket));
   }
 
+  @Test
+  void should_store_package_in_locker2_and_return_ticket_when_store_package_given_locker2_has_1_space_and_locker_1and3_are_full() {
+    // Arrange
+    List<Locker> lockers = Arrays.asList(
+        createLocker(5, 0),
+        createLocker(3, 1),
+        createLocker(3, 0));
+    SuperLockerRobot superLockerRobot = new SuperLockerRobot(lockers);
+    Package pack = new Package();
+
+    // Act
+    Ticket ticket = superLockerRobot.store(pack);
+
+    // Assert
+    assertNotNull(ticket);
+    assertTrue(lockers.get(1).isPackageAvailable(ticket));
+  }
+
+  @Test
+  void should_store_package_in_locker_2or3_and_return_ticket_when_store_package_given_locker1_is_full_and_locker_2and3_have_same_vacancy_rate() {
+    // Arrange
+    List<Locker> lockers = Arrays.asList(
+        createLocker(5, 0),
+        createLocker(3, 2),
+        createLocker(6, 4));
+    SuperLockerRobot superLockerRobot = new SuperLockerRobot(lockers);
+    Package pack = new Package();
+
+    // Act
+    Ticket ticket = superLockerRobot.store(pack);
+
+    // Assert
+    assertNotNull(ticket);
+    assertTrue(lockers.get(1).isPackageAvailable(ticket) || lockers.get(2).isPackageAvailable(ticket));
+  }
+
   private Locker createLocker(int capacity, int availableSpace) {
     Locker locker = new Locker(capacity);
     for(int i = 0; i < capacity - availableSpace; i++) {
